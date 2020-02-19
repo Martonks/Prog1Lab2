c878d0554c470c4345710dba3f0bd5f41fb9102e
martonks <martycal.@Outlook.com>
2020-02-19 10:21:24 -0500

    Lab 2

--- BMICal.java
+++ BMICal.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class BMICal{
 
-	 public static void main(String[] args) {
-	 	Scanner scan = new Scanner(System.in);
-	 	System.out.println("Enter your weight: ");
-	 	double weight = scan.nextDouble();
-	 	System.out.println("Enter your height: ");
-	 	double height = scan.nextDouble();
-	 	
-	 	double weightPounds = weight *
-	 	double heightFeet = height *
-	 	
-	 }
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your weight: ");
+		double weight = scan.nextDouble();
 
+		System.out.println("Enter your height");
+		double height = scan.nextDouble();
+
+		double weightKg = weight/2.205;
+		double heightMeters = height/3.281;
+
+		double bmi = weightKg/(heightMeters*heightMeters);
+
+		DecimalFormat bmiCal = new DecimalFormat("0.00");
+		System.out.println("Your BMI is "+bmiCal.format(bmi));
+
+
+	}
 }
-
-    
