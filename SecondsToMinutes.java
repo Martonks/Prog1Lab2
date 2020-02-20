10e8bbce594b5d9b03864cae2732b4d564fc0974
martonks <martycal@outlook.com>
2020-02-20 14:19:59 -0500

    Lab2

---
+++ SecondsToMinutes.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SecondsToMinutes {
+
+    public static void main(String[] args){
+        Scanner Scanner = new Scanner(System.in);
+
+        System.out.print("Input seconds: ");
+        	
+        	int seconds = scan.nextInt();
+        	int segundos = seconds % 60;
+        	int minutos = seconds / 60;
+
+        System.out.println(minutos+":"+segundos);
+
+    }    
+ }
