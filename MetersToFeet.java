a882e8d18c9b887a553d38d0848d68479fc72426
martonks <martycal@Outlook.com>
2020-02-20 20:05:38 -0500

    Lab 2

--- MetersToFeet.java
+++ MetersToFeet.java
@@ -3,11 +3,13 @@ public class MetersToFeet {
 
     public static void main(String[] args) {
     	Scanner scan = new Scanner(System.in);
-    	System.out.println("How many meters would you like to convert to feet?");
-		double meters = scan.nextDouble();
-    	double feet;
 
-    	feet = meters * 3.28084;
+    	System.out.println("How many meters would you like to convert to feet?");
+		
+			double meters = scan.nextDouble();
+    		double feet;
+
+    		feet = meters * 3.28084;
 
     	System.out.println(meters+" meters is equal to "+feet+" feet"); 	
          
