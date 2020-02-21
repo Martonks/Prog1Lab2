8424745ea5e6e4927468a1cd013b506156db9603
martonks <martycal@Outlook.com>
2020-02-20 20:03:02 -0500

    Lab 2

--- MyAgeIs.java
+++ MyAgeIs.java
@@ -3,10 +3,11 @@ public class MyAgeIs {
 
     public static void main(String[] args) {
     	Scanner scan = new Scanner(System.in);
+
     	System.out.println("What year were you born in?");
-    	int yearOfBirth = scan.nextInt();
-    	int currentYear = 2020;
     	
+        int yearOfBirth = scan.nextInt();
+    	int currentYear = 2020;
     	int age;
 
     	age = currentYear - yearOfBirth;
