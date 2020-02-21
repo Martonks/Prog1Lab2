f3386aa798f9c68a6e2c1945196ec95a5fcd258c
martonks <martycal@Outlook.com>
2020-02-20 19:53:59 -0500

    Lab 2

--- SecondsToMinutes.java
+++ SecondsToMinutes.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class SecondsToMinutes {
 
     public static void main(String[] args){
-        Scanner Scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.print("Input seconds: ");
         	
